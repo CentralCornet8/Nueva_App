ea6d0e5a5c474dcf518a02675f47cdb2755ac1b2
CentralCornet8 <gabasteitor-300@hotmail.com>
2017-03-06 06:00:13 -0700

    bola

--- app/src/main/java/com/example/gabriel/nueva_app/MainActivity.java
+++ app/src/main/java/com/example/gabriel/nueva_app/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //hola
-    //byronth vas a repetir progra 3!! por segunda vez :(
+    //Bola
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
